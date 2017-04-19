471773198e44ac3cc3c572eb51e91805948db1d7
mrRosset <marius.rosset@epfl.ch>
2017-04-19 19:45:25 +0200

    add a few tests for branch instructions

--- Armv4TEmulator/Tests/Disassembler_Tests.cpp
+++ Armv4TEmulator/Tests/Disassembler_Tests.cpp
@@ -13,6 +13,17 @@ std::string Disassemble(u32 instr) {
 	return Disassembler::Disassemble(ir);
 }
 
-TEST_CASE("Disassemble Data Processing", "[Disassembler]") {
+TEST_CASE("Disassemble Data Processing instr", "[Disassembler]") {
 	REQUIRE(Disassemble(0x2201230F) == "andcs r2, r1, #1006632960");
+}
+
+TEST_CASE("Disassemble Branch instr", "[Disassembler]") {
+	REQUIRE(Disassemble(0xEA000000) == "b +#8");
+	REQUIRE(Disassemble(0xEAFFFFFE) == "b +#0");
+	REQUIRE(Disassemble(0xEA000076) == "b +#480");
+	REQUIRE(Disassemble(0xEB000000) == "bl +#8");
+	REQUIRE(Disassemble(0xEBFFFFFE) == "bl +#0");
+	REQUIRE(Disassemble(0xEB000076) == "bl +#480");
+	REQUIRE(Disassemble(0x312FFF15) == "bxcc r5");
+	REQUIRE(Disassemble(0x312FFF1E) == "bxcc lr");
 }
