931ac3a411e5bfda96daceed5e5129d08d55c092
mrRosset <marius.rosset@epfl.ch>
2017-04-24 15:56:25 +0200

    fix error in decodin simple load store

--- Armv4TEmulator/Decoder/Decoder.cpp
+++ Armv4TEmulator/Decoder/Decoder.cpp
@@ -162,7 +162,7 @@ void Decoder::Decode_Status_Register(IR_ARM& ir, u32 instr) {
 }
 
 void Decoder::Decode_Load_Store_W_UB(IR_ARM& ir, u32 instr) {
-
+	unsigned t = (instr >> 20) & 0b10111;
 	switch ((instr >> 20) & 0b10111) {
 	case 0b00000: ir.instr = Instructions::STR; break;
 	case 0b00001: ir.instr = Instructions::LDR; break;
@@ -172,7 +172,7 @@ void Decoder::Decode_Load_Store_W_UB(IR_ARM& ir, u32 instr) {
 	case 0b00101: ir.instr = Instructions::LDRB; break;
 	case 0b00110: ir.instr = Instructions::STRBT; break;
 	case 0b00111: ir.instr = Instructions::LDRBT; break;
-	case 0b10001: ir.instr = Instructions::STR; break;
+	case 0b10001: ir.instr = Instructions::LDR; break;
 	case 0b10010: ir.instr = Instructions::STR; break;
 	case 0b10011: ir.instr = Instructions::LDR; break;
 	case 0b10100: ir.instr = Instructions::STRB; break;
