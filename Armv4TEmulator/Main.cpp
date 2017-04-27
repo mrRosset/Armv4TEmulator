bc5e48d92997059f56bb1bdf03dea7a442f3de61
mrRosset <marius.rosset@epfl.ch>
2017-04-27 23:41:22 +0200

    add option to run disassembler on a binary file

--- Armv4TEmulator/Main.cpp
+++ Armv4TEmulator/Main.cpp
@@ -1,8 +1,36 @@
 #include <string>
+#include <iostream>
+
+#include "Common.h"
+#include "Decoder\IR.h"
+#include "Decoder\Decoder.h"
+#include "Disassembler\Disassembler.h"
 
 #define CATCH_CONFIG_RUNNER
 #include "catch/catch.hpp"
 
+
+bool loadFile(std::string path, std::vector<u8>& data) {
+
+	std::ifstream stream(path, std::ios::binary);
+	if (!stream) {
+		return false;
+	}
+
+	stream.seekg(0, std::ios::end);
+	u64 length = stream.tellg();
+	stream.seekg(0, std::ios::beg);
+
+	data.resize(length);
+
+	for (u64 i = 0; i < length; i++)
+	{
+		data[i] = stream.get();
+	}
+
+	stream.close();
+	return true;
+}
 
 int main(int argc, char* argv[])
 {
@@ -12,7 +40,23 @@ int main(int argc, char* argv[])
 			int result = Catch::Session().run(argc, argv);
 			return (result < 0xff ? result : 0xff);
 		}
+		else {
+			std::vector<u8> code;
+			if (!loadFile(argv[1], code)) {
+				return -1;
+			}
+
+			for (int i = 0; i < code.size() / 4; i+=4) {
+				u32 instr = (code[i+3] << 24) | (code[i+2] << 16) | (code[i+1] << 8) | code[i];
+				IR_ARM ir;
+				Decoder::Decode(ir, instr);
+				std::cout << Disassembler::Disassemble(ir) << "\r\n";
+			}
+
+
+		}
 	}
+
 
 	return 0;
 }
