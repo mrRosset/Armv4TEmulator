a1a67063e801efee4f3052f955da63eb735a15a2
mrRosset <marius.rosset@epfl.ch>
2017-04-27 23:05:14 +0200

    remove unwanted file

--- Armv4TEmulator/runsdvui.cmd
+++
@@ -1,2 +0,0 @@
-cd /d "C:\Users\marius\documents\visual studio 2015\Projects\Armv4TEmulator\Armv4TEmulator" &msbuild "Armv4TEmulator.vcxproj" /t:sdvViewer /p:configuration="Debug" /p:platform=x64
-exit %errorlevel% 
