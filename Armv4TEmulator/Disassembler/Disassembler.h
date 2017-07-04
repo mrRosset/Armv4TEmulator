d5182f37d263377dec0376b966b74a7e555f57da
mrRosset <marius.rosset@epfl.ch>
2017-07-04 15:10:05 +0200

    Generalize the disassembling of conditions

--- Armv4TEmulator/Disassembler/Disassembler.h
+++ Armv4TEmulator/Disassembler/Disassembler.h
@@ -9,7 +9,7 @@ namespace Disassembler {
 	//ARM
 	std::string Disassemble(IR_ARM& ir);
 	std::string Disassemble_Reg(u32 reg);
-	std::string Disassemble_Cond(IR_ARM& ir);
+	std::string Disassemble_Cond(Conditions& cond);
 	std::string Disassemble_Shifter_Operand(Shifter_op & so, bool negatif);
 	std::string Disassemble_Shifter_Operand(Shifter_op & so);
 	std::string Disassemble_Branch_Offset(u32 operand);
@@ -21,4 +21,5 @@ namespace Disassembler {
 
 	//THUMB
 	std::string Disassemble(IR_Thumb& ir);
+	std::string Disassemble_Signed_Offset(s16 offset);
 }
--- Armv4TEmulator/Disassembler/Disassembler_Thumb.cpp
+++ Armv4TEmulator/Disassembler/Disassembler_Thumb.cpp
@@ -3,5 +3,17 @@
 
 
 std::string Disassembler::Disassemble(IR_Thumb& ir) {
+	
+	switch (ir.instr) {
+
+	//Branch Instructions
+	case TInstructions::B_cond: return "b" + Disassemble_Cond(ir.cond) + " " + Disassemble_Signed_Offset(SignExtend<s32>(ir.operand1 << 1, 9) + 4);
+	}
+	
 	return std::string();
+}
+
+std::string Disassembler::Disassemble_Signed_Offset(s16 offset) {
+	std::string sign = offset >= 0 ? "+" : "-";
+	return sign + "#" + std::to_string(abs(offset));
 }
