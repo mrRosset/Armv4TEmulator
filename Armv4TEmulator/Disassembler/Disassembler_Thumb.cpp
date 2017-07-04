d4fcb0b2643c5c756b423a315a596c363df0b068
mrRosset <marius.rosset@epfl.ch>
2017-07-04 15:27:26 +0200

    add disassembling of unconditional branch

--- Armv4TEmulator/Disassembler/Disassembler_Thumb.cpp
+++ Armv4TEmulator/Disassembler/Disassembler_Thumb.cpp
@@ -8,6 +8,7 @@ std::string Disassembler::Disassemble(IR_Thumb& ir) {
 
 	//Branch Instructions
 	case TInstructions::B_cond: return "b" + Disassemble_Cond(ir.cond) + " " + Disassemble_Signed_Offset(SignExtend<s32>(ir.operand1 << 1, 9) + 4);
+	case TInstructions::B_imm: return "b " + Disassemble_Signed_Offset(SignExtend<s32>(ir.operand1 << 1, 12) + 4);
 	}
 	
 	return std::string();
