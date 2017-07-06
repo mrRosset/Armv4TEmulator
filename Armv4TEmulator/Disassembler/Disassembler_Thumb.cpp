f604b0bd0b2b0a2332fde136177a6038dbec8294
mrRosset <marius.rosset@epfl.ch>
2017-07-06 21:20:02 +0200

    add data processing instructions to disassembling

--- Armv4TEmulator/Disassembler/Disassembler_Thumb.cpp
+++ Armv4TEmulator/Disassembler/Disassembler_Thumb.cpp
@@ -23,12 +23,27 @@ std::string Disassembler::Disassemble(IR_Thumb& ir) {
 	case TInstructions::LSR_imm: return "lsr " + Disassemble_Reg(ir.operand1) + ", " + Disassemble_Reg(ir.operand2) + ", #" + std::to_string(ir.operand3);
 	case TInstructions::ASR_imm: return "asr " + Disassemble_Reg(ir.operand1) + ", " + Disassemble_Reg(ir.operand2) + ", #" + std::to_string(ir.operand3);
 	
-	case TInstructions::MOV_imm: return "mov " + Disassemble_Reg(ir.operand2) + ", #" + std::to_string(ir.operand1);
-	case TInstructions::CMP_imm: return "cmp " + Disassemble_Reg(ir.operand2) + ", #" + std::to_string(ir.operand1);
+	case     TInstructions::MOV_imm: return "mov " + Disassemble_Reg(ir.operand2) + ", #" + std::to_string(ir.operand1);
+	case     TInstructions::CMP_imm: return "cmp " + Disassemble_Reg(ir.operand2) + ", #" + std::to_string(ir.operand1);
 	case TInstructions::ADD_lar_imm: return "add " + Disassemble_Reg(ir.operand2) + ", #" + std::to_string(ir.operand1);
 	case TInstructions::SUB_lar_imm: return "sub " + Disassemble_Reg(ir.operand2) + ", #" + std::to_string(ir.operand1);
 
-
+	case     TInstructions::AND: return "and " + Disassemble_Reg(ir.operand1) + ", " + Disassemble_Reg(ir.operand2);
+	case     TInstructions::EOR: return "eor " + Disassemble_Reg(ir.operand1) + ", " + Disassemble_Reg(ir.operand2);
+	case TInstructions::LSL_reg: return "lsl " + Disassemble_Reg(ir.operand1) + ", " + Disassemble_Reg(ir.operand2);
+	case TInstructions::LSR_reg: return "lsr " + Disassemble_Reg(ir.operand1) + ", " + Disassemble_Reg(ir.operand2);
+	case TInstructions::ASR_reg: return "asr " + Disassemble_Reg(ir.operand1) + ", " + Disassemble_Reg(ir.operand2);
+	case     TInstructions::ADC: return "adc " + Disassemble_Reg(ir.operand1) + ", " + Disassemble_Reg(ir.operand2);
+	case     TInstructions::SBC: return "sbc " + Disassemble_Reg(ir.operand1) + ", " + Disassemble_Reg(ir.operand2);
+	case     TInstructions::ROR: return "ror " + Disassemble_Reg(ir.operand1) + ", " + Disassemble_Reg(ir.operand2);
+	case     TInstructions::TST: return "tst " + Disassemble_Reg(ir.operand1) + ", " + Disassemble_Reg(ir.operand2);
+	case     TInstructions::NEG: return "neg " + Disassemble_Reg(ir.operand1) + ", " + Disassemble_Reg(ir.operand2);
+	case TInstructions::CMP_reg: return "cmp " + Disassemble_Reg(ir.operand1) + ", " + Disassemble_Reg(ir.operand2);
+	case     TInstructions::CMN: return "cmn " + Disassemble_Reg(ir.operand1) + ", " + Disassemble_Reg(ir.operand2);
+	case     TInstructions::ORR: return "orr " + Disassemble_Reg(ir.operand1) + ", " + Disassemble_Reg(ir.operand2);
+	case     TInstructions::MUL: return "mul " + Disassemble_Reg(ir.operand1) + ", " + Disassemble_Reg(ir.operand2);
+	case     TInstructions::BIC: return "bic " + Disassemble_Reg(ir.operand1) + ", " + Disassemble_Reg(ir.operand2);
+	case     TInstructions::MVN: return "mvn " + Disassemble_Reg(ir.operand1) + ", " + Disassemble_Reg(ir.operand2);	
 	}
 
 	return std::string();
